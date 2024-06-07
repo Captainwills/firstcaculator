af479d6a8f74bb452e8ee49f785dc4e719eec01b
Captainwills <ohiozuagodswill>
2024-06-07 17:39:54 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const display = document.getElementById("display");
+
+function appendToDisplay(input) {
+    display.value += input;
+}
+function calculate() {
+
+    try{
+    display.value = eval(display.value);
+    }
+    catch(error){
+        display.value= "Invalid";
+    }
+    
+
+}
+function clearDisplay() {
+    display.value = "";
+
+}
